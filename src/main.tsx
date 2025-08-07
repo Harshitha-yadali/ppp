ea6ca50a3fc21816b4fedf2e96dd621785100daf
Harshitha-yadali <harshithayadali30@gmail.com>
2025-08-07 19:59:10 +0530

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,18 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { AuthProvider } from './contexts/AuthContext'; // ✅ Import this
+import { AuthProvider } from './contexts/AuthContext';
 import { ThemeProvider } from './contexts/ThemeContext';
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 
 createRoot(document.getElementById('root')!).render(
-  
+  <StrictMode>
     <ThemeProvider>
-      <AuthProvider> {/* ✅ Wrap App with AuthProvider */}
-        <App />
+      <AuthProvider>
+        <BrowserRouter> {/* Wrap App with BrowserRouter */}
+          <App />
+        </BrowserRouter>
       </AuthProvider>
     </ThemeProvider>
-  
+  </StrictMode>
 );
